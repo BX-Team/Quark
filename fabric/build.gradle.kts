0744db7788bdd0ae45d1bb3b1b227b854d9dbdd3
NONPLAY <76615486+NONPLAYT@users.noreply.github.com>
2025-09-16 01:34:35 +0300

    feat: add fabric platform

---
+++ fabric/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    `quark-deps`
+    `quark-java`
+    `quark-publish`
+}
+
+repositories {
+    maven("https://maven.fabricmc.net")
+}
+
+dependencies {
+    api(project(":quark-core"))
+
+    compileOnly("net.fabricmc:fabric-loader:0.14.21")
+    compileOnly("org.slf4j:slf4j-api:2.0.5")
+}
